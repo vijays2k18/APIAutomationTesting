7f7d2d14738672b891d1921fcae509877cd1ff52
vijays2k18 <vijays2k18@gmail.com>
2024-11-20 15:09:59 +0530

    updated

--- src/test/java/APIBasicsTesting/APIAutomationBasics.java
+++ src/test/java/APIBasicsTesting/APIAutomationBasics.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import Payloads.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 
@@ -15,9 +16,14 @@ public class APIAutomationBasics {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		
 		//Add Place API
-		given().queryParam("key", "qaclick123").header("Content-Type","application/json").body(payload.Addplace())
+		String addAPIResponse = given().queryParam("key", "qaclick123").header("Content-Type","application/json").body(payload.Addplace())
 		.when().post("/maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200);
+		.then().assertThat().statusCode(200).extract().asString();
+		System.out.println(addAPIResponse);
+		// Extracting Place Id from Response
+		JsonPath js = new JsonPath(addAPIResponse);
+		String placeId = js.getString("place_id");
+		System.out.println(placeId);
 		
 	}
 
